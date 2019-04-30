6319d4d2af900bff7898d38d130f63cc5e68471c
STRIDELY\kthakkar <kunalt.stridely@gmail.com>
2019-04-30 17:00:28 +0530

    Initial commit by Kunal

---
+++ MvcBootstrap/MvcBootstrap/MvcBootstrap/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public JsonResult GetCountries()
+        {
+            var Countries = new List<string>();
+            Countries.Add("Australia");
+            Countries.Add("India");
+            Countries.Add("Russia");
+            return Json(Countries, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public ActionResult PostTime(string selectedtTime)
+        {
+            //put a checkpoint here and interrogate selectedTime and work with it
+            return Json(new
+            {
+                aValue = "aValueRet"
+            }
+                   , @"application/json");
+        }
+
+        public ActionResult CountryList()
+        {
+            return View("Getcountries");
+        }
+
+
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcBootstrap/MvcBootstrap/MvcBootstrap/Models/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcBootstrap.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Please enter username")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter valid email.")]
+        [MinLength(50, ErrorMessage = "Length must be greater than 50.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter new password")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password required")]
+        [Compare("NewPassword", ErrorMessage = "Password doesn't match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
